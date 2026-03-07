32627c72db3936bed587675002cd5c201b40928a
Zadyy <nyambayar2014@gmail.com>
2026-03-08 07:34:27 +0800

    Implemented GroupId wrapper

--- SocialMediaPlatform.Core/Domain/ID/GroupId.cs
+++ SocialMediaPlatform.Core/Domain/ID/GroupId.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace SocialMediaPlatform.Core.Domain.ID
 {
+    /// <summary>
+    /// Грүпийн ID дугаарыг ялгахад зориулсан wrapper класс
+    /// </summary>
     public class GroupId
     {
+        /// <summary>
+        /// Грүпийн ID дугаарын утга
+        /// </summary>
+        public required uint Value { get; init; }
+
+        public override string ToString() => Value.ToString();
     }
 }
