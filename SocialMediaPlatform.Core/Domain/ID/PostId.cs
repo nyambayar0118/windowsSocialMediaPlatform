c1039aba446045a99a381e6d7c3382d7a1f04c13
Zadyy <nyambayar2014@gmail.com>
2026-03-08 07:37:16 +0800

    Implemented PostId wrapper

--- SocialMediaPlatform.Core/Domain/ID/PostId.cs
+++ SocialMediaPlatform.Core/Domain/ID/PostId.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SocialMediaPlatform.Core.Domain.ID
+﻿namespace SocialMediaPlatform.Core.Domain.ID
 {
+    /// <summary>
+    /// Постийн ID дугаарыг ялгахад зориулсан wrapper класс
+    /// </summary>
     public class PostId
     {
+        /// <summary>
+        /// Постийн ID дугаарын утга
+        /// </summary>
+        public required uint Value { get; init; }
+
+        public override string ToString() => Value.ToString();
     }
 }
