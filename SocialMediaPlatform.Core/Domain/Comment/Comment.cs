66566ba97a686b92130846f5b03d3ba3bf52246c
Zadyy <nyambayar2014@gmail.com>
2026-03-08 08:52:42 +0800

    Implemented Comment abstract class

--- SocialMediaPlatform.Core/Domain/Comment/Comment.cs
+++ SocialMediaPlatform.Core/Domain/Comment/Comment.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using SocialMediaPlatform.Core.Domain.ID;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SocialMediaPlatform.Core.Domain.Comment
 {
-    public class Comment
+    /// <summary>
+    /// Comment-ийн үндсэн абстракт класс
+    /// </summary>
+    public abstract class Comment<TCommentType>
     {
+        /// <summary>ID дугаар</summary>
+        public required CommentId Id { get; init; }
+
+        /// <summary>Бичсэн хэрэглэгчийн ID дугаар</summary>
+        public required UserId AuthorId { get; init; }
+
+        /// <summary>Агуулга</summary>
+        public required string Content { get; init; }
+
+        /// <summary>Үүсгэсэн огноо</summary>
+        public DateTime CreatedAt { get; init; } = DateTime.Now;
+
+        /// <summary>Comment-ийн төрөл</summary>
+        public required TCommentType Type { get; init; }
     }
 }
