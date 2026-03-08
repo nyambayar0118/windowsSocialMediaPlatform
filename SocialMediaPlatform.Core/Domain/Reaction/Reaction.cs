b66d325d43001aed2244388eefd8c6fafca6e7f7
Zadyy <nyambayar2014@gmail.com>
2026-03-08 15:27:12 +0800

    Implemented Reaction

--- SocialMediaPlatform.Core/Domain/Reaction/Reaction.cs
+++ SocialMediaPlatform.Core/Domain/Reaction/Reaction.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.Enum;
+using SocialMediaPlatform.Core.Domain.ID;
 
 namespace SocialMediaPlatform.Core.Domain.Reaction
 {
-    public class Reaction
+    /// <summary>
+    /// Reaction-ий үндсэн абстракт класс
+    /// </summary>
+    public abstract class Reaction<TReactionType>
     {
+        /// <summary>Reaction-ийг дарсан зүйлийн ID дугаар</summary>
+        public required uint TargetId { get; init; }
+
+        /// <summary>Reaction-ийг дарсан зүйлийн төрөл</summary>
+        public required ReactionTargetType TargetType { get; init; }
+
+        /// <summary>Бичсэн хэрэглэгчийн ID дугаар</summary>
+        public required UserId AuthorId { get; init; }
+
+        /// <summary>Үүсгэсэн огноо</summary>
+        public DateTime CreatedAt { get; init; } = DateTime.Now;
+
+        /// <summary>Reaction-ий төрөл</summary>
+        public required TReactionType Type { get; init; }
     }
 }
