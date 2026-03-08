d7d630af854eee7467d02e58104756e9375606f5
Zadyy <nyambayar2014@gmail.com>
2026-03-08 15:29:39 +0800

    Implemented User

--- SocialMediaPlatform.Core/Domain/User/User.cs
+++ SocialMediaPlatform.Core/Domain/User/User.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.ID;
 
 namespace SocialMediaPlatform.Core.Domain.User
 {
-    public class User
+    /// <summary>
+    /// User-ийн үндсэн абстракт класс
+    /// </summary>
+    public abstract class User<TUserType>
     {
+        /// <summary>ID дугаар</summary>
+        public required UserId Id { get; init; }
+
+        /// <summary>Хэрэглэгчийн нэр</summary>
+        public required string Username { get; set; }
+
+        /// <summary>Цахим шуудан хаяг</summary>
+        public required string Email { get; set; }
+
+        /// <summary>Нууц үг</summary>
+        public required string Password { get; set; }
+
+        /// <summary>Хэрэглэгч үүсгэсэн огноо</summary>
+        public DateTime CreatedAt { get; init; } = DateTime.Now;
+
+        /// <summary>Хэрэглэгчийн төрөл</summary>
+        public required TUserType Type { get; init; }
     }
 }
