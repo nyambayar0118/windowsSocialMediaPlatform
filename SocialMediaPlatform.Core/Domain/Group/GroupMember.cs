71540c9e5ebb8cc654f0586a1419a54ff1f3fa46
Zadyy <nyambayar2014@gmail.com>
2026-03-08 15:20:51 +0800

    Implemented GroupMemeber

--- SocialMediaPlatform.Core/Domain/Group/GroupMember.cs
+++ SocialMediaPlatform.Core/Domain/Group/GroupMember.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.ID;
 
 namespace SocialMediaPlatform.Core.Domain.Group
 {
-    public class GroupMember
+    /// <summary>
+    /// Group-ийн гишүүнчлэлийн үндсэн класс
+    /// </summary>
+    public class GroupMember<TPrivilege>
     {
+        /// <summary>Group-ийн ID дугаар</summary>
+        public required GroupId GroupId { get; init; }
+
+        /// <summary>Гишүүний ID дугаар</summary>
+        public required UserId UserId { get; init; }
+
+        /// <summary>Гишүүний эрх</summary>
+        public required TPrivilege Role { get; set; }
+
+        /// <summary>Group-т элссэн огноо</summary>
+        public DateTime JoinedAt { get; init; } = DateTime.Now;
     }
 }
