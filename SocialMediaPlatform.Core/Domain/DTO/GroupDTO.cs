fb58fb05b7e300ca8b12fa14e79c48fbd933af71
Zadyy <nyambayar2014@gmail.com>
2026-03-08 08:33:53 +0800

    Implemented GroupDTO

--- SocialMediaPlatform.Core/Domain/DTO/GroupDTO.cs
+++ SocialMediaPlatform.Core/Domain/DTO/GroupDTO.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.ID;
 
 namespace SocialMediaPlatform.Core.Domain.DTO
 {
-    public class GroupDTO
-    {
-    }
+    /// <summary>
+    /// Group классын объектын Data Transfer Object
+    /// </summary>
+    /// <param name="Id">ID дугаар</param>
+    /// <param name="Name">Нэр</param>
+    /// <param name="Description">Дэлгэрэнгүй</param>
+    /// <param name="OwnerId">Group-ийн эзэмшигчийн ID дугаар</param>
+    public record GroupDTO(
+        GroupId Id,
+        string Name,
+        string Description,
+        UserId OwnerId
+    );
 }
