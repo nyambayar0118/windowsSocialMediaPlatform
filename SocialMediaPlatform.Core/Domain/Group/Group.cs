92d32fc6ddd23019110eb81e02cffe29c12d8e0a
Zadyy <nyambayar2014@gmail.com>
2026-03-08 15:18:10 +0800

    Implemented Group

--- SocialMediaPlatform.Core/Domain/Group/Group.cs
+++ SocialMediaPlatform.Core/Domain/Group/Group.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SocialMediaPlatform.Core.Domain.ID;
 
 namespace SocialMediaPlatform.Core.Domain.Group
 {
+    /// <summary>
+    /// Group-ийн үндсэн класс
+    /// </summary>
     public class Group
     {
+        /// <summary>
+        /// ID дугаар
+        /// </summary>
+        public required GroupId Id { get; init; }
+        /// <summary>
+        /// Group-ийн нэр
+        /// </summary>
+        public required string Name { get; set; }
+        /// <summary>
+        /// Group-ийн дэлгэрэнгүй
+        /// </summary>
+        public required string Description { get; set; }
+        /// <summary>
+        /// Group-ийн үүсгэсэн огноо
+        /// </summary>
+        public DateTime CreatedAt { get; init; } = DateTime.Now;
+        /// <summary>
+        /// Group-ийн эзэмшигчийн ID дугаар
+        /// </summary>
+        public required UserId OwnerId { get; set; }
     }
 }
